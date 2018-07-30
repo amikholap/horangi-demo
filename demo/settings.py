bafb6a5a27caaaac447006e397564fd536130955
Ales Mikholap <ales.mikholap@gmail.com>
2018-07-31 02:31:38 +0800

    More production-friendly settings

--- demo/settings.py
+++ demo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '90^1@fakoz*dr_g&1_r=yae@c#ui)bi%==xa=mtl$t-61hjs(p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
